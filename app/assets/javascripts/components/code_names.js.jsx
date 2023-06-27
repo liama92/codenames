4ff61f5266ce04af196af34372cc884e5da8448d
Liam A <liamedia92@gmail.com>
2023-06-27 15:25:30 +0100

    add endgame

--- app/assets/javascripts/components/code_names.js.jsx
+++ app/assets/javascripts/components/code_names.js.jsx
@@ -1,6 +1,6 @@
 var CodeNames = createReactClass({
   getInitialState: function() {
-    return {cards: this.props.cards}
+    return {cards: this.props.cards, gameOver: false}
   },
   render: function() {
     const player = {
@@ -23,12 +23,28 @@ var CodeNames = createReactClass({
         .then((data) => {
           data.json().then((json) => {
             this.setState({cards: json.cards})
+            checkEndGame()
           })
         })
     }
+    if (!this.state.gameOver) setTimeout(updateGame, 1000);
 
-    setTimeout(updateGame, 1000);
 
+    const checkEndGame = () => {
+      if (!this.state.gameOver) {
+        const redCards = this.state.cards.filter((c) => c.colour === 'red')
+        const blueCards = this.state.cards.filter((c) => c.colour === 'blue')
+        if (redCards.every((c) => c.revealed)) {
+          window.alert('red has won')
+          this.setState({gameOver: true})
+  
+        } else if (blueCards.every((c) => c.revealed)) {
+          window.alert('blue has won')
+          this.setState({gameOver: true})
+  
+        }
+      }
+    }
 
     return (
       <React.Fragment>
